5e697982efe00dbaf08abe3eba9d9e9f43477dc7
sofia chavez <sofiachavez@MacBook-Air-de-sofia.local>
2025-07-01 12:25:08 -0500

    sunat

--- config/greenter.php
+++ config/greenter.php
@@ -3,8 +3,8 @@ use Greenter\Ws\Services\SunatEndpoints;
 use Greenter\See;
 
 $see = new See();
+$see->setCertificate(file_get_contents(base_path('sunat/certificados/certificate.pem')));
 
-$see->setCertificate(file_get_contents(storage_path('app/certificados/certificate.pem')));
 $see->setService(SunatEndpoints::FE_BETA);
 $see->setClaveSOL('20000000001', 'MODDATOS', 'moddatos');
 
