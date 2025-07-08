9de95a5fb84a3157c83a38c91341d13d78fae999
Pablo lupu <sofiachavez@MacBook-Air-de-sofia.local>
2025-07-08 13:27:18 -0500

    subir certificado

---
+++ resources/js/pages/panel/certificado/indexcertificado.vue
@@ -0,0 +1,36 @@
+<template>
+    <Head title="Clientes" />
+    <AppLayout>
+        <div>
+            <template v-if="isLoading">
+                <Espera/>
+            </template>
+            <template v-else>
+                <div class="card">
+                                    <SubirCertificado :refresh="refreshKey"/>
+
+             </div>
+            </template>
+        </div>
+    </AppLayout>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import AppLayout from '@/layout/AppLayout.vue';
+import { Head } from '@inertiajs/vue3';
+import Espera from '@/components/Espera.vue';
+import SubirCertificado from './Desarrollo/SubirCertificado.vue';
+
+const isLoading = ref(true);
+const refreshKey = ref(0);
+
+function refrescarListado() {
+    refreshKey.value++;
+}
+onMounted(() => {
+    setTimeout(() => {
+        isLoading.value = false;
+    }, 1000);
+});
+</script>
